958b49e3bdf91cad3441bd67e340b9e580e69e35
somalingck <136596008+somalingck@users.noreply.github.com>
2023-08-12 13:11:59 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.meth());
+
+        B b1 = new B();
+        System.out.println(b1.meth());
+    }
+
 }
